7f3e8fb772dc2324e06047c23804883e01978698
Vasilii Chugunov <naukomania@gmail.com>
2016-12-27 14:37:19 +0300

    final version

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 'use strict';
 // Ссылка на серверную часть приложения
-var serviceBase = 'http://server.client';
+var serviceBase = 'http://server.local/';
 // Основной модуль приложения и его компоненты
 var yii2AngApp = angular.module('yii2AngApp', [
   'ngRoute',
-  'yii2AngApp.site'
+  'yii2AngApp.site',
+  'yii2AngApp.film'
 ]);
-// рабочий модуль
-var yii2AngApp_site = angular.module('yii2AngApp.site', ['ngRoute']);
  
 yii2AngApp.config(['$routeProvider', function($routeProvider) {
   // Маршрут по-умолчанию
   $routeProvider.otherwise({redirectTo: '/site/index'});
 }]);
+
+// рабочий модуль
+var yii2AngApp_site = angular.module('yii2AngApp.site', ['ngRoute']);
+var yii2AngApp_film = angular.module('yii2AngApp.film', ['ngRoute']);
